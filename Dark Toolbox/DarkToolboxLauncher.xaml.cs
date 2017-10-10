4c015d81b950ab884113f57c5c13bd9bf310878b
Ege E. Turan <darklive_@live.com>
2017-10-10 20:37:40 +0300

    Visuals

--- Dark Toolbox/DarkToolboxLauncher.xaml.cs
+++ Dark Toolbox/DarkToolboxLauncher.xaml.cs
@@ -24,25 +24,29 @@ namespace Dark_Toolbox
 
         private void Navigate(object sender, RoutedEventArgs e)
         {
-            if (((Button)sender).Content.ToString() == "Schedule")
+            if (((Button)sender).Name.ToString() == "EventScheduler")
             {
                 ToolScheduler NewPAGE = new ToolScheduler();
                 NewPAGE.Show();
             }
-            else if (((Button)sender).Content.ToString() == "Packer")
+            else if (((Button)sender).Name.ToString() == "UpdatePacker")
             {
                 ToolPacker NewPAGE = new ToolPacker();
                 NewPAGE.Show();
             }
-            else if (((Button)sender).Content.ToString() == "Gamma")
+            else if (((Button)sender).Name.ToString() == "GammaModifier")
             {
                 ToolGamma NewPAGE = new ToolGamma();
                 NewPAGE.Show();
             }
-            else if (((Button)sender).Content.ToString() == "Core")
+            else if (((Button)sender).Name.ToString() == "CPUOptimizer")
             {
                 ToolCPUCore NewPAGE = new ToolCPUCore();
                 NewPAGE.Show();
+            }
+            else if (((Button)sender).Name.ToString() == "Placeholder")
+            {
+                MessageBox.Show("Probably");
             }
         }
     }
